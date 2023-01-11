6c9efc093ce7816704f58a99fb610f38a1f1c0e9
karmelnaveen <karmelnaveen@gmail.com>
2023-01-11 09:07:02 +0530

    palindrome or not

--- FirstIdeaProject/src/com/assingments/first_java_3.java
+++ FirstIdeaProject/src/com/assingments/first_java_3.java
@@ -5,8 +5,16 @@ import java.util.Scanner;
 public class first_java_3 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        System.out.println("Enter a number to find fibonaaci values");
         int n = in.nextInt();
         fibo(n);
+        System.out.println("Enter a word to find whether it is palindrome or not");
+        String word = in.next();
+        if(isPalindrome(word)){
+            System.out.println(word +" is a palindrome");
+        }else {
+            System.out.println(word +" is not a palindrome");
+        }
     }
     static void fibo(int n){
         int first = 0;
@@ -18,7 +26,15 @@ public class first_java_3 {
             first = second;
             second = nth;
         }
+        System.out.println("\n");
 
+    }
+    static boolean isPalindrome(String word){
+        StringBuilder reversed = new StringBuilder();
+        for (int i = 1 ; i<=word.length();i++){
+            reversed.append(word.charAt(word.length() - i));
+        }
+        return word.equalsIgnoreCase(reversed.toString());
     }
     
 }
