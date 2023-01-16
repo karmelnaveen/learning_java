7a25a99cc098efafd86ec2340aa9f785566be538
karmelnaveen <karmelnaveen@gmail.com>
2023-01-16 17:15:22 +0530

    6 to 10

--- FirstIdeaProject/src/com/assingments/conditional_loops_2.java
+++ FirstIdeaProject/src/com/assingments/conditional_loops_2.java
@@ -15,13 +15,45 @@ public class conditional_loops_2 {
         System.out.print("Side :");
         double c = in.nextDouble();
         areaOfEQTriangle(c);
+        System.out.println("To find the perimeter of the circle Enter its side in cm");
+        System.out.print("radius :");
+        double d = in.nextDouble();
+        perimeterOfCircle(d);
+        System.out.println("To find the perimeter of the equilateral triangle Enter its side in cm");
+        System.out.print("Side :");
+        double e = in.nextDouble();
+        perimeterOfEQTriangle(e);
+        System.out.println("To find the perimeter of the parallelogram enter its side in cm");
+        System.out.print("Side :");
+        double f = in.nextDouble();
+        System.out.print("Base :");
+        double g = in.nextDouble();
+        perimeterOfParallelogram(f,g);
     }
+    //6
     static void areaOfRhombus(double a,double b){
         double ans = (a*b)/2;
         System.out.println("The area of the rhombus is "+ans+"cm");
     }
+    //7
     static void areaOfEQTriangle(double side){
         double ans = (Math.sqrt(3)/4)*side*side;
         System.out.println("The area of the equilateral triangle is "+ans+"cm");
     }
+    //8
+    static void perimeterOfCircle(double radius){
+        double ans = 2*Math.PI*radius;
+        System.out.println("The perimeter of the circle is "+ans+"cm");
+    }
+    //9
+    static void perimeterOfEQTriangle(double side){
+        double ans = 3*side;
+        System.out.println("The perimeter of the equilateral triangle is "+ans+"cm");
+    }
+    //10
+    static void perimeterOfParallelogram(double side,double base){
+        double ans = 2*(side+base);
+        System.out.println("The perimeter of the parallelogram triangle is "+ans+"cm");
+    }
+
 }
