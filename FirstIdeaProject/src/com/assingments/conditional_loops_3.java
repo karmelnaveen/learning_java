4bd08aaa34c97421273a9eb7cfd7592c4260abf8
karmelnaveen <karmelnaveen@gmail.com>
2023-01-17 17:20:02 +0530

    11 to 15

--- FirstIdeaProject/src/com/assingments/conditional_loops_3.java
+++ FirstIdeaProject/src/com/assingments/conditional_loops_3.java
@@ -1,11 +1,6 @@
 package com.assingments;
 
 import java.util.Scanner;
-//Perimeter Of Rectangle
-//        Perimeter Of Square
-//        Perimeter Of Rhombus
-//        Volume Of Cone Java Program
-//        Volume Of Prism
 public class conditional_loops_3 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -19,15 +14,44 @@ public class conditional_loops_3 {
         System.out.print("Enter its side :");
         double a = in.nextDouble();
         perimeterOfSquare(a);
+        System.out.println("To find the perimeter of rhombus...");
+        System.out.print("Enter its side :");
+        double b = in.nextDouble();
+        perimeterOfRhombus(b);
+        System.out.println("To find the volume of cone...");
+        System.out.print("Enter its height :");
+        double h = in.nextDouble();
+        System.out.print("Enter its radius :");
+        double r = in.nextDouble();
+        volumeOfCone(h,r);
+        System.out.println("To find the volume of prism...");
+        System.out.print("Enter its base :");
+        double b2 = in.nextDouble();
+        System.out.print("Enter its height :");
+        double h2 = in.nextDouble();
+        volumeOfPrism(b2,h2);
     }
     //11
     static void perimeterOfRectangle(double l,double w){
-        System.out.println("The perimeter of the rectangle is"+2*(l+w));
+        System.out.println("The perimeter of the rectangle is "+2*(l+w));
     }
     //12
     static void perimeterOfSquare(double a){
-        System.out.println("The perimeter of the square is"+4*a);
+        System.out.println("The perimeter of the square is "+4*a);
     }
+    //13
+    static void perimeterOfRhombus(double a){
+        perimeterOfSquare(a); //same formula as that of square
+    }
+    //14
+    static void volumeOfCone(double height,double radius){
+        System.out.println("The perimeter of the square is "+Math.PI*(radius*radius)*(height/2));
+    }
+    //15
+    static void volumeOfPrism(double base,double height){
+        System.out.println("The volume of the prism is "+base*height);
+    }
+
 
 
 }
