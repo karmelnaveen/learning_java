d2eb05020e1d312d3a9ee57fa360df662c103fe2
karmelnaveen <karmelnaveen@gmail.com>
2023-01-18 07:42:24 +0530

    16 to 20

--- FirstIdeaProject/src/com/assingments/conditional_loops_4.java
+++ FirstIdeaProject/src/com/assingments/conditional_loops_4.java
@@ -1,9 +1,7 @@
 package com.assingments;
 
 import java.util.Scanner;
-//        Volume Of Pyramid
-//        Curved Surface Area Of Cylinder
-//        Total Surface Area Of Cube
+
 public class conditional_loops_4 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -25,6 +23,16 @@ public class conditional_loops_4 {
         System.out.print("Enter its height :");
         double h = in.nextDouble();
         volumeOfPyramid(l,w,h);
+        System.out.println("To Find the curved surface area of a cylinder..");
+        System.out.print("Enter its radius:");
+        double r2 = in.nextDouble();
+        System.out.print("Enter its  height:");
+        double h2 = in.nextDouble();
+        curvedSurfaceArea(r2,h2);
+        System.out.println("To Find the surface area of a cube..");
+        System.out.print("Enter its side:");
+        double a = in.nextDouble();
+        surfaceAreaOfCube(a);
     }
 
     //16
@@ -39,5 +47,12 @@ public class conditional_loops_4 {
     static void volumeOfPyramid(double length,double width,double height){
         System.out.println("The volume of the pyramid is "+(length*width*height)/3);
     }
-
+    //19
+    static void curvedSurfaceArea(double radius, double height){
+        System.out.println("The curved Surface Area of the cylinder is "+2*(Math.PI*radius*height));
+    }
+    //20
+    static void surfaceAreaOfCube(double side){
+        System.out.println("The curved Surface Area of the cube is "+6*Math.pow(side,2));
+    }
 }
