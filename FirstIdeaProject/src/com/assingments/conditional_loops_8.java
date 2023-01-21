c1bc3f00a3a563d0dbdea558f282d630ef8ec922
karmelnaveen <karmelnaveen@gmail.com>
2023-01-21 14:35:38 +0530

    11 to 15

--- FirstIdeaProject/src/com/assingments/conditional_loops_8.java
+++ FirstIdeaProject/src/com/assingments/conditional_loops_8.java
@@ -1,9 +1,6 @@
 package com.assingments;
 
 import java.util.Scanner;
-//        Calculate Average Marks
-//        Sum Of N Numbers
-//        Armstrong Number In Java
 //        Find Ncr & Npr
 public class conditional_loops_8 {
     public static void main(String[] args) {
@@ -27,11 +24,52 @@ public class conditional_loops_8 {
         System.out.print("CS :");
         double cs = in.nextDouble();
         AverageMarks(english,tamil,maths,cs);
+        System.out.println("Enter two number to find armstrong number between them");
+        System.out.print("First :");
+        int start = in.nextInt();
+        System.out.print("Second :");
+        int end = in.nextInt();
+        for(int i = start;i<=end;i++){
+            if(Armstrong(i)){
+                System.out.print(i+"\t");
+            }
+        }
+        System.out.print("Enter a number to find its ncr and npr, n =");
+        int n = in.nextInt();
+        System.out.print("r =");
+        int r = in.nextInt();
+        Ncr_Npr(n,r);
     }
+    //11
     static void CompoundInterest(double principle,double roi,double time){
         System.out.println("The amount is "+(principle*Math.pow(1+(roi/100),time)));
     }
+    //12
     static void AverageMarks(double english,double tamil,double maths,double cs){
         System.out.println("The average is "+((english+tamil+maths+cs)/4));
+    }
+    //13 is already done
+    //14
+    static boolean Armstrong(int n){
+        int original = n;
+        int sum = 0;
+        while(n>0){
+            int i = n%10;
+            sum+=Math.pow(i,3);
+            n = n/10;
+        }
+        return original == sum;
+    }
+    //15
+    static void Ncr_Npr(int n,int r){
+        System.out.println("The NCR is :"+((fact(n)) / (fact(r)*fact(n-r))));
+        System.out.println("The NPR is :"+((fact(n)) / (fact(n-r))));
+    }
+    static int fact(int n){
+        int fact = 1;
+        for(int i =1;i<=n;i++){
+            fact*=fact*i;
+        }
+        return fact;
     }
 }
