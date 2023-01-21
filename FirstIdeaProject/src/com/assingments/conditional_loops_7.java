f07a070218672a3fb9fa25161f39eb41fcb901ab
karmelnaveen <karmelnaveen@gmail.com>
2023-01-21 12:47:00 +0530

    depreciation

--- FirstIdeaProject/src/com/assingments/conditional_loops_7.java
+++ FirstIdeaProject/src/com/assingments/conditional_loops_7.java
@@ -1,7 +1,6 @@
 package com.assingments;
 
 import java.util.Scanner;
-//        Power In Java
 //        Calculate Depreciation of Value
 //        Calculate Batting Average
 //        Calculate CGPA Java Program
@@ -20,12 +19,26 @@ public class conditional_loops_7 {
         System.out.print("Power :");
         int power = in.nextInt();
         Power(base,power);
+        System.out.println("To Find the depreciation of an asset, Enter the following,");
+        System.out.print("Total value of the asset :");
+        double value = in.nextDouble();
+        System.out.print("Rate of depreciation of that asset class :");
+        double rod = in.nextDouble();
+        System.out.print("Time duration in years :");
+        double years = in.nextDouble();
+        Depreciation(value,rod,years);
     }
+    //6
     static void CommissionPercentage(double amount,double commission){
         System.out.println("The commission percentage is"+((commission*100)/amount)+"%");
     }
+    //7
     static void Power(int base, int power){
         System.out.println("The answer is "+Math.pow(base,power));
+    }
+    //8
+    static void Depreciation(double value, double rateOfDepreciation, double years){
+        System.out.println("The answer is "+value * Math.pow((1 - rateOfDepreciation / 100), years));//Formula of Depreciation
     }
 
 
