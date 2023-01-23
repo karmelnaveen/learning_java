e720069695585a53b55fbcc7441f337dc6141721
karmelnaveen <karmelnaveen@gmail.com>
2023-01-23 16:54:34 +0530

    functions

---
+++ SecondIdeaProject/src/Methods/methods.java
@@ -0,0 +1,9 @@
+package Methods;
+
+import java.util.Scanner;
+//Define two methods to print the maximum and the minimum number respectively among three numbers entered by the user.
+public class methods {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+    }
+}
