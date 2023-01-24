083e21687bbafea8164094f0ead8040723fe9344
karmelnaveen <karmelnaveen@gmail.com>
2023-01-24 10:28:29 +0530

    grade

--- SecondIdeaProject/src/Methods/methods_5.java
+++ SecondIdeaProject/src/Methods/methods_5.java
@@ -2,8 +2,8 @@ package Methods;
 
 import java.util.Scanner;
 
-//Define a method to find out if a number is prime or not.
-//Write a program that will ask the user to enter his/her marks (out of 100).
+//7.Define a method to find out if a number is prime or not.
+//8.Write a program that will ask the user to enter his/her marks (out of 100).
 //Define a method that will display grades according to the marks entered as below:
 public class methods_5 {
     public static void main(String[] args) {
@@ -11,6 +11,19 @@ public class methods_5 {
         System.out.print("Enter a number to find whether its prime or not :");
         int num = in.nextInt();
         isPrime(num);
+        System.out.println("To find your grades enter your marks subject wise out of 100,");
+        System.out.print("English :");
+        double english = in.nextDouble();
+        String score = Grade(english);
+        System.out.println("You grade in english is "+score);
+        System.out.print("Tamil :");
+        double tamil = in.nextDouble();
+        String score2 = Grade(tamil);
+        System.out.println("You grade in tamil is "+score2);
+        System.out.print("Maths :");
+        double maths = in.nextDouble();
+        String score3 = Grade(maths);
+        System.out.println("You grade in maths is "+score3);
     }
     static void isPrime(int num){
         if(num == 0){
@@ -28,5 +41,30 @@ public class methods_5 {
             }
         }
         System.out.println(num+" is a prime Number");
+    }
+    static String Grade(double marks){
+        if(marks >=91 && marks <= 100){
+            return "AA";
+        }
+        if(marks >=81 && marks <= 90){
+            return "AB";
+        }
+        if(marks >=71 && marks <= 80){
+            return "BB";
+        }
+
+        if(marks >=61 && marks <= 70){
+            return "BC";
+        }
+        if(marks >=51 && marks <= 60){
+            return "CD";
+        }
+        if(marks >=41 && marks <= 50){
+            return "DD";
+        }
+        if(marks <=40){
+            return "Fail";
+        }
+        return "Enter a valid mark to get results";
     }
 }
