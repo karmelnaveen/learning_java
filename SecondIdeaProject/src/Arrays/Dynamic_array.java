f3b6f75386df95f101631dfc07ac1bcf6aa7199b
karmelnaveen <karmelnaveen@gmail.com>
2023-01-25 09:43:03 +0530

    dynamic array

---
+++ SecondIdeaProject/src/Arrays/Dynamic_array.java
@@ -0,0 +1,15 @@
+package Arrays;
+
+import java.util.ArrayList;
+
+public class Dynamic_array {
+    public static void main(String[] args) {
+        ArrayList<Integer> list =  new ArrayList<>(5);
+        list.add(123);
+        list.add(456);
+        list.add(789);
+        list.add(127);
+
+        System.out.println(list);
+    }
+}
